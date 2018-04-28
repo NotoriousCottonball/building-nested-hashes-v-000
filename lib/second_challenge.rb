382e7bb91dd68259a577ec1312646ddbcf90d0a0
George  Pianka <gpianka1@gmail.com>
2018-04-28 05:24:47 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,15 +1,19 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => {}
       
     }, 
     :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :
       
-      }}
+    }
+  }
 
   
 end
